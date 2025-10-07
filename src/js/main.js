b97cd97fcdbccf81bf62acb92f6a9b473a9ae805
OscarDu93 <oscar.tomasetti@studentfr.ch>
2025-10-07 17:12:21 +0200

    swiper

--- src/js/main.js
+++ src/js/main.js
@@ -1,16 +1,12 @@
 import Swiper, { Navigation, Pagination } from "swiper";
 
 Swiper.use([Navigation, Pagination]);
-
-var swiper = new Swiper(".swiper-container", {
-  scrollbar: {
-    el: ".swiper-scrollbar",
-    hide: true,
+const swiper = new Swiper(".swiper-container", {
+  pagination: {
+    el: ".swiper-pagination",
   },
-});
-
-var link = $(".pagination__link");
-link.on("click", function () {
-  link.removeClass("is_active");
-  $(this).addClass("is_active");
+  navigation: {
+    nextEl: ".carousel-next",
+    prevEl: ".carousel-prev",
+  },
 });
