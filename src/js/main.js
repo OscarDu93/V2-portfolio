5f3bc8fc5e3e38f730ee3726595c8f21ce159d20
OscarDu93 <oscar.tomasetti@studentfr.ch>
2025-10-17 15:52:35 +0200

    swiper dots

--- src/js/main.js
+++ src/js/main.js
@@ -2,11 +2,19 @@ import Swiper, { Navigation, Pagination } from "swiper";
 
 Swiper.use([Navigation, Pagination]);
 const swiper = new Swiper(".swiper-container", {
+  loop: true,
   pagination: {
     el: ".swiper-pagination",
+    clickable: true,
+    renderBullet: function (index, className) {
+      const thumbs = [".swiper-slide"];
+      return `<span class="${className}">
+                
+              </span>`;
+    },
   },
   navigation: {
-    nextEl: ".carousel-next",
-    prevEl: ".carousel-prev",
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
   },
 });
